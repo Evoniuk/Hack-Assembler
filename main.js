eaa11da638676a3a859a3dd7d635384d134c98b0
evoniuk <jack.evoniuk@gmail.com>
2020-06-13 23:45:16 -0700

    added file download capability

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 document.getElementById('fileInput').addEventListener('change', function() {
+  const file = this.files[0];
+  const fileName = file.name.substring(0, file.name.indexOf('.'));
+
   const reader = new FileReader();
-  reader.onload = () => displayCode(reader.result);
-  reader.readAsText(this.files[0]);
+  reader.onload = () => displayCode(reader.result, fileName);
+  reader.readAsText(file);
 })
 
 document.getElementById('inputCode').addEventListener('click', function() {
@@ -9,16 +12,21 @@ document.getElementById('inputCode').addEventListener('click', function() {
   displayCode(codeInput);
 })
 
-function displayCode (code) {
+function displayCode (code, fileName) {
   const outputOriginal = document.getElementById('outputOriginal');
   const outputArea = document.getElementById('output');
   outputOriginal.textContent = '';
   outputArea.textContent = '';
 
   const processedCode = process(code);
-  for (let i = 0; i < processedCode[0].length; i++) {
-    outputOriginal.textContent += processedCode[0][i] + '\n';
-    outputArea.textContent += processedCode[1][i] + '\n';
+
+  if (processedCode[1].length > 50)
+    download(fileName + '.hack', processedCode[1].join('\n'));
+  else {
+    for (let i = 0; i < processedCode[0].length; i++) {
+      outputOriginal.textContent += processedCode[0][i] + '\n';
+      outputArea.textContent += processedCode[1][i] + '\n';
+    }
   }
 
   if (outputArea.textContent !== '') {
@@ -31,4 +39,15 @@ function process(code) {
   const parsedCode = parse(code);
   const unSymbolized = handleSymbols(parsedCode);
   return [unSymbolized, assemble(unSymbolized)];
+}
+
+function download(filename, text) {
+  const element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
 }
